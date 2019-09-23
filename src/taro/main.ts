9796c3d0223f2495e5acc419bc854cc476b972be
ABA Games <cs8k-cyu@asahi-net.or.jp>
2019-09-23 22:15:08 +0900

    Add hearts

--- src/taro/main.ts
+++ src/taro/main.ts
@@ -10,7 +10,7 @@ import * as text from "../util/text";
 import * as terminal from "../util/terminal";
 import * as sga from "../util/simpleGameActor";
 import { Vector } from "../util/vector";
-import { wrap, range } from "../util/math";
+import { wrap, range, clamp } from "../util/math";
 import * as sss from "sounds-some-sounds";
 
 type State = "title" | "inGame" | "gameOver";
@@ -34,13 +34,21 @@ function init() {
   sss.init(0);
   sga.setActorClass(Actor);
   text.defineSymbols(charPatterns, "A");
-  initInGame();
+  initTitle();
 }
 
 function update() {
-  view.clear();
   terminal.clear();
+  view.clear();
+  view.context.fillStyle = "black";
+  view.context.fillRect(0, 43, 120, 17);
+  arrowSpawnTicks--;
+  if (arrowSpawnTicks < 0) {
+    arrowSpawnTicks = (100 / gameSpeed) * random.get(0.75, 1);
+    sga.spawn(arrow);
+  }
   updateFunc[state]();
+  sga.update();
   if (score > 0) {
     terminal.print(`${score}`, 0, 0, { colorPattern: "lllllll" });
   }
@@ -48,27 +56,31 @@ function update() {
   ticks++;
 }
 
-let arrowSpawnTicks: number;
+let lifeHearts: any[];
+let gameSpeed = 1;
+let arrowSpawnTicks = 0;
+let nextExtendScore: number;
+let isExtending: boolean;
 
 function initInGame() {
   state = "inGame";
+  sga.reset();
   ticks = 0;
+  score = 0;
+  lifeHearts = range(2).map(i => sga.spawn(heart, i));
+  gameSpeed = 1;
   arrowSpawnTicks = 0;
+  nextExtendScore = 100;
+  isExtending = false;
   sga.spawn(taro);
 }
 
-let gameSpeed: number;
-
 function updateInGame() {
-  gameSpeed = 1 + ticks * 0.0005;
-  arrowSpawnTicks--;
-  if (arrowSpawnTicks < 0) {
-    arrowSpawnTicks = (100 / gameSpeed) * random.get(0.5, 1);
-    sga.spawn(arrow);
+  gameSpeed += 0.0002;
+  if (score >= nextExtendScore) {
+    nextExtendScore += 100;
+    isExtending = true;
   }
-  view.context.fillStyle = "black";
-  view.context.fillRect(0, 43, 120, 17);
-  sga.update();
 }
 
 function taro(a: Actor) {
@@ -76,11 +88,16 @@ function taro(a: Actor) {
   let isDead = false;
   let coinTicks = 0;
   let coinMultiplier = 1;
+  let invincibleTicks = 0;
   a.vel.set(0.3, 0);
   a.pos.set(60, 40);
   a.addUpdater(() => {
     if (isDead) {
       a.vel.y += 0.2;
+      if (a.pos.y > 66) {
+        a.remove();
+        initGameOver();
+      }
       return;
     }
     a.pos.clamp(0, 120, 0, 60);
@@ -101,15 +118,27 @@ function taro(a: Actor) {
       }
     }
     a.rotationPattern = a.vel.x > 0 ? "k" : "n";
-    sga.pool.get(arrow).forEach((ar: Actor) => {
-      if (a.testCollision(ar)) {
-        isDead = true;
-        a.rotationPattern = a.vel.x > 0 ? "j" : "b";
-        a.animInterval = 9999999;
-        a.vel.x *= -2;
-        a.vel.y = -3 / Math.sqrt(gameSpeed);
-      }
-    });
+    if (invincibleTicks <= 0) {
+      sga.pool.get(arrow).forEach((ar: Actor) => {
+        if (a.testCollision(ar)) {
+          if (lifeHearts.length > 0) {
+            lifeHearts[lifeHearts.length - 1].dead();
+            lifeHearts.pop();
+            invincibleTicks = 100;
+          } else {
+            isDead = true;
+            a.rotationPattern = a.vel.x > 0 ? "j" : "b";
+            a.animInterval = 9999999;
+            a.vel.x *= -2;
+            a.vel.y = -3 / Math.sqrt(gameSpeed);
+          }
+        }
+      });
+    } else {
+      invincibleTicks--;
+      a.colorPattern =
+        invincibleTicks > 0 && invincibleTicks % 30 < 15 ? "w" : "l";
+    }
     sga.pool.get(coin).forEach((c: Actor) => {
       if (a.testCollision(c)) {
         score += coinMultiplier;
@@ -128,45 +157,58 @@ function taro(a: Actor) {
         terminal.print(ms, 20 - ms.length, 0, { colorPattern: "lllll" });
       }
     }
+    sga.pool.get(extendHeart).forEach((e: Actor) => {
+      if (a.testCollision(e)) {
+        lifeHearts.push(sga.spawn(heart, lifeHearts.length));
+        e.remove();
+      }
+    });
   });
 }
 
-let arrowAddingPattern = 0;
-
 function arrow(a: Actor) {
-  arrowAddingPattern = wrap(arrowAddingPattern + 1, 0, 6);
+  const ap = random.getInt(6);
   a.animChar = "C";
-  switch (arrowAddingPattern) {
+  a.collidingRect.set(4, 1);
+  switch (ap) {
     case 0:
-    case 3:
-      a.pos.set(-3, arrowAddingPattern === 0 ? 40 : random.get(20, 30));
+    case 1:
+      a.pos.set(-3, ap === 0 ? 40 : random.get(20, 30));
       a.vel.set(0.4, 0);
       break;
-    case 1:
-    case 4:
-      a.pos.set(123, arrowAddingPattern === 1 ? 40 : random.get(20, 30));
+    case 2:
+    case 3:
+      a.pos.set(123, ap === 2 ? 40 : random.get(20, 30));
       a.vel.set(-0.4, 0);
       a.rotationPattern = "n";
       break;
-    case 2:
+    case 4:
     case 5:
-      a.pos.set(random.get(0, 120), -3);
+      a.pos.set(random.get(20, 100), -3);
       a.vel.set(0, 0.3);
       a.rotationPattern = "l";
+      a.collidingRect.set(1, 4);
       break;
   }
-  if (random.get() < 0.2) {
+  if (ap < 4 && isExtending) {
+    if (lifeHearts.length < 2) {
+      const ep = new Vector(a.pos);
+      ep.x -= a.vel.x * 25;
+      ep.y -= a.vel.y * 25;
+      sga.spawn(extendHeart, ep, a.vel, a.rotationPattern);
+    }
+    isExtending = false;
+  } else if (random.get() < 0.2) {
     const cp = new Vector(a.pos);
     cp.x -= a.vel.x * 5;
     cp.y -= a.vel.y * 5;
     range(9).forEach(i => {
       cp.x -= a.vel.x * 15;
       cp.y -= a.vel.y * 15;
-      sga.spawn(coin, cp, a.vel);
+      sga.spawn(coin, cp, a.vel, a.rotationPattern);
     });
   }
   a.animInterval = 20;
-  a.collidingRect.set(4, 1);
   a.addUpdater(() => {
     if (a.pos.x < -6 || a.pos.x > 126 || a.pos.y > 40) {
       a.remove();
@@ -174,11 +216,12 @@ function arrow(a: Actor) {
   });
 }
 
-function coin(a: Actor, p: Vector, v: Vector) {
+function coin(a: Actor, p: Vector, v: Vector, rp: string) {
   a.pos.set(p);
   a.vel.set(v);
   a.animChar = "E";
   a.animInterval = 15;
+  a.rotationPattern = rp;
   a.addUpdater(() => {
     if (
       (a.vel.x < 0 && a.pos.x < -6) ||
@@ -188,6 +231,46 @@ function coin(a: Actor, p: Vector, v: Vector) {
       a.remove();
     }
   });
+}
+
+function extendHeart(a: Actor, p: Vector, v: Vector, rp: string) {
+  a.pos.set(p);
+  a.vel.set(v);
+  a.animChar = "G";
+  a.animInterval = 9999999;
+  a.rotationPattern = rp;
+  a.addUpdater(() => {
+    if (
+      (a.vel.x < 0 && a.pos.x < -6) ||
+      (a.vel.x > 0 && a.pos.x > 126) ||
+      a.pos.y > 40
+    ) {
+      a.remove();
+    }
+  });
+}
+
+function heart(a: Actor & { dead: Function }, i: number) {
+  a.pos.set(i * 6 + 3, 56);
+  a.colorPattern = "w";
+  a.animChar = "G";
+  a.animInterval = 45;
+  a.animInterval = 9999999;
+  let isDead = false;
+  a.addUpdater(() => {
+    if (isDead) {
+      a.vel.y += 0.2;
+      if (a.pos.y > 63) {
+        a.remove();
+      }
+    }
+  });
+  a.dead = () => {
+    isDead = true;
+    a.rotationPattern = "n";
+    a.vel.x = 1;
+    a.vel.y = -2 / Math.sqrt(gameSpeed);
+  };
 }
 
 class Actor extends sga.Actor {
@@ -201,6 +284,7 @@ class Actor extends sga.Actor {
   animIndexVel = 1;
   rotationPattern = "k";
   collidingRect = new Vector(4, 4);
+  colorPattern = "l";
 
   update() {
     super.update();
@@ -218,7 +302,7 @@ class Actor extends sga.Actor {
       this.pos.x - 3,
       this.pos.y - 3,
       {
-        colorPattern: "l",
+        colorPattern: this.colorPattern,
         symbolPattern: "s",
         rotationPattern: this.rotationPattern
       }
@@ -237,10 +321,11 @@ class Actor extends sga.Actor {
 
 function initTitle() {
   state = "title";
-  sga.reset();
+  ticks = 0;
 }
 
 function updateTitle() {
+  terminal.print("TOBITARO", 6, 3, { colorPattern: "lllllllll" });
   if (isJustPressed) {
     initInGame();
   }
@@ -249,11 +334,15 @@ function updateTitle() {
 function initGameOver() {
   state = "gameOver";
   clearJustPressed();
+  ticks = 0;
 }
 
 function updateGameOver() {
-  if (isJustPressed) {
-    state = "inGame";
+  terminal.print("GAME OVER", 5, 3, { colorPattern: "lllllllll" });
+  if (ticks > 20 && isJustPressed) {
+    initInGame();
+  } else if (ticks > 300) {
+    initTitle();
   }
 }
 
@@ -293,5 +382,12 @@ w
  w
 www
  w
+`,
+  `
+ w w
+wwwww
+wwwww
+ www
+  w
 `
 ];
