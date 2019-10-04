3d825a429637b5b1f034bf6df156ab19b4c035f2
ABA Games <cs8k-cyu@asahi-net.or.jp>
2019-10-04 16:32:09 +0900

    Export with module name

--- src/tobi/tobi.ts
+++ src/tobi/tobi.ts
@@ -3,10 +3,10 @@ import * as text from "./text";
 import * as terminal from "./terminal";
 import * as input from "./input";
 import * as sound from "sounds-some-sounds";
+import * as actor from "./actor";
+import * as debug from "./debug";
 import { Vector, VectorLike } from "./vector";
-export { view, text, terminal, input, sound, Vector, VectorLike };
-export * from "./actor";
-export * from "./debug";
+export { view, text, terminal, input, sound, actor, debug, Vector, VectorLike };
 export { clamp, wrap, isInRange, range, stableSort } from "./math";
 export { Random } from "./random";
 
